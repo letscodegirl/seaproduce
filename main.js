4fc79ac326c916de4fdc3416b9095b2b7ea6da7a
chloe <chloe.laureate@gmail.com>
2024-04-17 19:02:02 +0900

    a lot of trials

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log(`First one to find this get bonus points. extra if you can tell the mentor's with out anyone else seeing it :D`)
-
 
 $('.multiple-items').slick({
   infinite: true,
@@ -28,6 +26,34 @@ $('.multiple-items').slick({
   ]
 
 
+});
+
+// A $( document ).ready() block.
+$(document).ready(function () {
+
+  $(".hamburger-button").click(function () {
+
+    $(this).toggleClass('active');
+    $(".mobile-menu").fadeToggle();
+
+  });
+
+  // animate on scroll - activate
+  AOS.init({
+    duration: 500 // values from 0 to 3000, with step 50ms
+  });
 
 });
 
+
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false
+
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
+
+});
